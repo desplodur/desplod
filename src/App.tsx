a3d285799733db2761fb537fda0f8ac787236234
Laurenz Rudolph <Laurenz.Rudolph@outlook.de>
2022-11-10 16:42:17 +0100

    add button component, card component, browserrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
+import Card from './components/Card';
+
+const projects = [{
+  title: 'Capstone Project',
+  description: 'none',
+  tasks: ["Entwicklung einer Web Applikation zum Stellen und Beantworten von Fragen",
+    "Entwerfen der UI/UX mit Figma",
+    "Entwicklung und Implementierung von UI/UX mit React und Styled Components",
+    "Entwicklung und Implementierung des Backends und dessen Datenbank mit Hilfe von Node.js  und MongoDB",
+    "Realisierung von Real-Time Aktualisierung mithilfe von Optimistic Updates. ",
+    "Agile Entwicklung mittels der Scrum-Methode"
+],
+  picURL: '',
+  techstack: ["JavaScript", "React", "Node.js", "UI/UX", "Figma", "HTML", "CSS", "Storybook", "React Testing Library", "MongoDB", "Mongoose", "Styled Components", "React Query", "Pforte", "VSC"]
+}, {
+  title: 'Capstone Project',
+  description: 'none',
+  tasks: ["Entwicklung einer Web Applikation zum Stellen und Beantworten von Fragen",
+    "Entwerfen der UI/UX mit Figma",
+    "Entwicklung und Implementierung von UI/UX mit React und Styled Components",
+    "Entwicklung und Implementierung des Backends und dessen Datenbank mit Hilfe von Node.js  und MongoDB",
+    "Realisierung von Real-Time Aktualisierung mithilfe von Optimistic Updates. ",
+    "Agile Entwicklung mittels der Scrum-Methode"
+],
+  picURL: '',
+  techstack: ["JavaScript", "React", "Node.js", "UI/UX", "Figma", "HTML", "CSS", "Storybook", "React Testing Library", "MongoDB", "Mongoose", "Styled Components", "React Query", "Pforte", "VSC"]
+}];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {projects.map((project) => (
+        <Card title={project.title} description={project.description} tasks={project.tasks} picURL={project.picURL} techstack={project.techstack} />
+      ))}
     </div>
   );
 }
---
+++ src/components/Card/index.js
@@ -0,0 +1,14 @@
+import styles from './Card.module.css';
+
+export default function Card({ title, description, tasks, picURL, techstack, ...props }) {
+
+    console.log(props)
+    return <div className={styles.card} {...props}>
+            <img className={styles.card__image} src={picURL? picURL : "https://storage.googleapis.com/proudcity/mebanenc/uploads/2021/03/placeholder-image.png" } alt={title} />
+            <div className={styles.card__content}>
+                <h3 className={styles.card__title}>{title}</h3>
+                <p className={styles.card__description}>{description}</p>
+                {techstack.map((tech, index) => <span key={index} className={styles.card__span}>{tech + " "}</span>)}
+            </div>
+    </div>;
+    }
